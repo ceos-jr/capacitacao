1a2f081d22891102020424c3c65bed450ec7f7e2
taldoflemis <coderflemis@gmail.com>
2022-11-14 19:43:27 -0300

    feat: adding mod suggestion popover and user avatar in sugg

--- src/server/trpc/router/admin.ts
+++ src/server/trpc/router/admin.ts
@@ -21,7 +21,7 @@ export const adminRouter = router({
     return ctx.prisma.modSuggestion.findMany({
       include: {
         module: { select: { id: true, name: true } },
-        user: { select: { id: true, name: true } },
+        user: { select: { id: true, name: true, image: true } },
       },
     });
   }),
